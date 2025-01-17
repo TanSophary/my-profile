e639c8b45b6c3f0144e50445e5701beb7d7800e7
TanSophary <tansophary@gmail.com>
2025-01-17 14:28:40 +0700

    all

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import "./index.css";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import AnimatedBackground from "./components/Background";
+import Navbar from "./components/Navbar";
+import Portofolio from "./Pages/Portofolio";
+import WelcomeScreen from "./Pages/WelcomeScreen";
+import { AnimatePresence } from 'framer-motion';
+
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <About />
+          <Portofolio />
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <h1 className="Get text-xl pb-5 text-gray-500 text-center dark:text-gray-400">Get In Touch</h1>
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                  138st, Sangkat Teuk Thla, Khan Sensok, Phnom Penh
+              </span>
+              <a href="https://t.me/Pha_Rie" className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                  Phone: 0979534329
+              </a>
+              <a href="tansophary@gmail.com" className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                  Email: tansophary@gmail.comm
+              </a>
+              
+            </center>
+          </footer>
+        </>
+      )}
+    </>
+  );
+};
+
+const ProjectPageLayout = () => (
+  <>
+    <footer>
+      <center>
+        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+        <h1 className="Get text-xl pb-5 text-gray-500 text-center dark:text-gray-400">Get In Touch</h1>
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          138st, Sangkat Teuk Thla, Khan Sensok, Phnom Penh
+        </span>
+        <a href="https://t.me/Pha_Rie" className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          Phone: 0979534329
+        </a>
+        <a href="tansophary@gmail.com" className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          Email: tansophary@gmail.com
+        </a>       
+      </center>
+    </footer>
+  </>
+);
+
+function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  return (
+    <BrowserRouter basename="/my-profile">
+      <Routes>
+        <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
+        <Route path="/project/:id" element={<ProjectPageLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Project.jsx
@@ -0,0 +1,62 @@
+// Project.jsx
+const projects = [
+    {
+      id: 1,
+      Img: '/cball.jpg',
+      Title: 'Project 1',
+      Description: 'Description for Project 1',
+      Link: '/project/1'
+    },
+    {
+      id: 2,
+      Img: '/image2.jpg',
+      Title: 'Project 2',
+      Description: 'Description for Project 2',
+      Link: '/project/1'
+    },
+    {
+      id: 3,
+      Img: '/image3.jpg',
+      Title: 'Project 3',
+      Description: 'Description for Project 3',
+      Link: '/project/1'
+    },
+    {
+      id: 4,
+      Img: '/image4.jpg',
+      Title: 'Project 4',
+      Description: 'Description for Project 4',
+      Link: '/project/1'
+    },
+    {
+      id: 5,
+      Img: '/image5.jpg',
+      Title: 'Project 5',
+      Description: 'Description for Project 5',
+      Link: '/project/1'
+    },
+    {
+      id: 6,
+      Img: '/image6.jpg',
+      Title: 'Project 6',
+      Description: 'Description for Project 6',
+      Link: '/project/1'
+    },
+    {
+      id: 7,
+      Img: '/image7.jpg',
+      Title: 'Project 7',
+      Description: 'Description for Project 7',
+      Link: '/project/1'
+    },
+    {
+      id: 8,
+      Img: '/image8.jpg',
+      Title: 'Project 8',
+      Description: 'Description for Project 8',
+      Link: '/project/1'
+    },
+  ];
+  
+  export default projects;
+  
